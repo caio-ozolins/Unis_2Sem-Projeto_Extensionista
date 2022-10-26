f2749c507cc144875c8fe50f6537112b2ea93be5
Caio Ozolins Mendes <caio.ozolins@gmail.com>
2022-10-26 19:26:28 -0300

    Guessing Game - added default to function level

--- Guessing_Game/level.c
+++ Guessing_Game/level.c
@@ -6,6 +6,7 @@ void level(int* maxAttempts){
     printf(":  1.Easy 2.Medium 3.Hard  :\n");
     printf("+--------------------------+\n\n");
     printf("Choose the difficulty level.\n");
+    x:
     scanf("%d", &difficulty);
     switch (difficulty) {
         case 1:
@@ -14,8 +15,11 @@ void level(int* maxAttempts){
         case 2:
             *maxAttempts=9;
             break;
-        default:
+        case 3:
             *maxAttempts=6;
             break;
+        default:
+            printf("Invalid choice, try again:\n");
+            goto x;
     }
 }
