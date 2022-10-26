fba9c79b0164e3ef95d439956872a3ba78978a91
Caio Ozolins Mendes <caio.ozolins@gmail.com>
2022-10-26 09:10:39 -0300

    Added menu

---
+++ Guessing_Game/guessingGame.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "level.c"
+#include "attempt.c"
+#include "ordinal.c"
+
+void guessingGame(){
+
+    printf("\n\n");
+    printf("+==========================+\n");
+    printf(":      Welcome to the      :\n");
+    printf(":      Guessing Game!      :\n");
+    printf("+==========================+\n");
+    printf("\n\n");
+
+    int atpt = 1, maxAttempts, victory;
+
+    level(&maxAttempts);
+
+    attempt(&atpt, maxAttempts, &victory);
+
+    char *ordinals[] = { "st", "nd", "rd", "th" };
+
+    if (victory){
+        printf("+=========================+\n");
+        printf(":     Congratulations     :\n");
+        printf(": You won on the %d%s try. :\n", atpt, get_ordinal(ordinals, atpt));
+        printf("+=========================+\n");
+    } else{
+        printf("+==========================+\n");
+        printf(":   You lost, try again.   :\n");
+        printf("+==========================+\n");
+    }
+}
--- main.c
+++ main.c
@@ -1,34 +1,21 @@
 #include <stdio.h>
-#include "adivinhacao/level.c"
-#include "adivinhacao/attempt.c"
-#include "adivinhacao/ordinal.c"
+#include "Guessing_Game/guessingGame.c"
 
 int main(){
+    int game=0;
 
-    printf("\n\n");
-    printf("+==========================+\n");
-    printf(":      Welcome to the      :\n");
-    printf(":      Guessing Game!      :\n");
-    printf("+==========================+\n");
-    printf("\n\n");
+    printf("\n\n+====================+\n");
+    printf(":  1. Guessing Game  :\n");
+    printf(":  2. Hangman Game   :\n");
+    printf("+====================+\n\n");
+    scanf("%d", &game);
 
-    int atpt = 1, maxAttempts, victory;
-
-    level(&maxAttempts);
-
-    attempt(&atpt, maxAttempts, &victory);
-
-    char *ordinals[] = { "st", "nd", "rd", "th" };
-
-    if (victory){
-        printf("+=========================+\n");
-        printf(":     Congratulations     :\n");
-        printf(": You won on the %d%s try. :\n", atpt, get_ordinal(ordinals, atpt));
-        printf("+=========================+\n");
-    } else{
-        printf("+==========================+\n");
-        printf(":   You lost, try again.   :\n");
-        printf("+==========================+\n");
+    switch (game) {
+        case 1:
+            guessingGame();
+            break;
+        default:
+            printf("Game not found");
     }
 
     return 0;
