3c6924930f94e46cefb7b5edea1f298f0875f2a4
Caio Ozolins Mendes <caio.ozolins@gmail.com>
2022-12-05 22:06:00 -0300

    Tic Tac Toe comentarios

--- Tic_Tac_Toe/ticTacToe.c
+++ Tic_Tac_Toe/ticTacToe.c
@@ -8,32 +8,32 @@ char tic_tac_toe[3][4]={ // Matriz do jogo da velha
         {'4','5','6'},
         {'7','8','9'}
 };
-char validPositions[10]={'1', '2', '3', '4', '5', '6', '7', '8', '9', '\0'}; // Guarda as posições q ainda não foram escolhidas
-int turn = 0; // guarda
+char validPositions[10]={'1', '2', '3', '4', '5', '6', '7', '8', '9', '\0'}; // Guarda as posições que ainda não foram escolhidas
+int turn = 0; // guarda a quantidade de turnos
 
 void ticTacToe(){
 
-    while (!wonTicTacToe() && !tie()) {
-        char playerChosenTile='0';
+    while (!wonTicTacToe() && !tie()) { // loop enquanto ninguem tiver ganhado ou empatado
+        char playerChosenTile='0'; // variavel para guardar a escolha do player
 
         printTicTacToe();
         printf("Choose a tile:");
         scanf(" %c", &playerChosenTile);
         putInTicTacToe(playerChosenTile, PLAYER);
         turn++;
-        if (wonTicTacToe() != 0){
+        if (wonTicTacToe() != 0){ // Verifica se o player ganhou
             printTicTacToe();
             printf("You Won!");
             break;
         }
-        if (tie()){
+        if (tie()){ // Verifica se houve empate
             printTicTacToe();
             printf("It's a Tie!");
             break;
         }
-        computerTurn();
+        computerTurn(); // Turno do computador
         turn++;
-        if (wonTicTacToe() != 0){
+        if (wonTicTacToe() != 0){ // Verifica se o computador ganhou
             printTicTacToe();
             printf("You Lost!");
             break;
@@ -41,7 +41,7 @@ void ticTacToe(){
     }
 }
 
-void printTicTacToe(){
+void printTicTacToe(){ // Imprime o jogo da velha
     printf("     |     |     \n");
     printf("  %c  |  %c  |  %c\n",tic_tac_toe[0][0],tic_tac_toe[0][1],tic_tac_toe[0][2]);
     printf("-----|-----|-----\n");
@@ -51,17 +51,17 @@ void printTicTacToe(){
     printf("     |     |     \n");
 }
 
-void putInTicTacToe(char chosenTile, char playerSimbol){
+void putInTicTacToe(char chosenTile, char playerSimbol){ // Coloca o simbolo no local selecionado
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
             if (tic_tac_toe[i][j] == chosenTile)
                 tic_tac_toe[i][j] = playerSimbol;
         }
     }
-    removeFromArray(chosenTile);
+    removeFromValidPositions(chosenTile); // Retira o local escolhido das posições válidas
 }
 
-int wonTicTacToe(){
+int wonTicTacToe(){ // Verifica se alguem ganhou
     int x;
     if (tic_tac_toe[0][0] == tic_tac_toe[0][1] && tic_tac_toe[0][0] == tic_tac_toe[0][2])
         return tic_tac_toe[0][0] == PLAYER ? 1 : 2;
@@ -83,11 +83,11 @@ int wonTicTacToe(){
     return 0;
 }
 
-int tie(){
+int tie(){ // Verifica se houve empate
     return turn == 9;
 }
 
-void removeFromArray(char x){
+void removeFromValidPositions(char x){ // Retira o local escolhido das posições válidas
     int sizeArray = mySizeOf(validPositions);
     for (int i = 0; i < sizeArray; ++i) {
         if (validPositions[i] == x){
@@ -106,12 +106,12 @@ void swap(char* x, char* y){ // Troca dois valores de lugar
 
 void computerTurn(){
     srand(time(NULL));
-    int chosenTileIndex = rand() % (mySizeOf(validPositions));
+    int chosenTileIndex = rand() % (mySizeOf(validPositions)); // Escolhe uma posição aleatória entre as disponiveis
     char chosenTile = validPositions[chosenTileIndex];
     putInTicTacToe(chosenTile, COMPUTER);
 }
 
-int mySizeOf(char array[]){
+int mySizeOf(char array[]){ // Ve o tamanho do array
     int i=0;
     while (array[i] != '\0'){
         i++;
--- Tic_Tac_Toe/ticTacToe.h
+++ Tic_Tac_Toe/ticTacToe.h
@@ -8,7 +8,7 @@ void printTicTacToe();
 void putInTicTacToe(char chosenTile, char playerSimbol);
 int wonTicTacToe();
 int tie();
-void removeFromArray(char x);
+void removeFromValidPositions(char x);
 void swap(char* x, char* y);
 void computerTurn();
 int mySizeOf(char array[]);
